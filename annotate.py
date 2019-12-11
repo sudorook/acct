04f77e381e4e6d21f75dfe4a66b2c75298ec2576
sudorook <daemon@nullcodon.com>
2019-12-11 13:41:02 -0600

    add annotation script

---
+++ annotate.py
@@ -0,0 +1,269 @@
+#! /usr/bin/env python3
+""" Script for annotating sequences. """
+
+import os
+import re
+import sys
+import csv
+import argparse
+import sqlite3
+from Bio import SeqIO
+
+
+ACCESSION2TAXID = "accession2taxid.db"
+PFAM = "pfamseq.db"
+TAXONOMY = "taxonomy.db"
+NR = "nr.db"
+
+
+def parse_options():
+    """ Make the command line parser. """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-i",
+        "--input",
+        dest="input",
+        required=True,
+        help="Input sequences (FASTA format)",
+    )
+    parser.add_argument(
+        "-d",
+        "--dbdir",
+        dest="dbdir",
+        default=None,
+        help="Path to (all) SQLite databases",
+    )
+    parser.add_argument(
+        "-f",
+        "--format",
+        dest="format",
+        default="multiple",
+        help="Database of origin for ALL sequences (omit for multiple sources)",
+    )
+    return parser.parse_args()
+
+
+def load_sequences(sequence_file, sequence_format="fasta"):
+    """ Load the sequences for annotation. """
+    sequences = list(SeqIO.parse(sequence_file, sequence_format))
+    return sequences
+
+
+def query_database(
+    database_path, database_name, return_field_list, query_dict
+):
+    """ Wrapper for making SQLite queries. """
+    with sqlite3.connect(database_path) as conn:
+        cur = conn.cursor()
+        cmd = (
+            "SELECT "
+            + ",".join(["`" + field + "`" for field in return_field_list])
+            + " FROM "
+            + database_name
+            + " WHERE "
+            + " AND ".join(["`" + key + "` = ?" for key in query_dict.keys()])
+            + ";"
+        )
+        keys = tuple(query_dict.values())
+        cur.execute(cmd, keys)
+
+        res = cur.fetchall()
+
+        # Only return unique results
+        if res:
+            if len(res) == 1:
+                return res
+        return None
+
+
+def identify_database(record):
+    """ Identify (i.e. guess) the database where a sequence originated. """
+    header = record.id
+
+    database = None
+    if "jgi|" in header:
+        database = "jgi"
+    elif "XM_" in header:
+        database = "ncbi"
+    else:
+        database = "pfam"
+    return database
+
+
+def read_species_map(path):
+    """ Return jgi species map """
+    species_map = {}
+    with open(path) as handle:
+        csv_reader = csv.reader(handle, delimiter=",")
+        next(csv_reader)
+        for row in csv_reader:
+            species_map[row[6]] = row[0]
+    return species_map
+
+
+def main():
+    """ Main """
+    options = parse_options()
+    if not options.dbdir:
+        try:
+            os.environ(["SQLITEDB"])
+        except KeyError as error:
+            sys.exit("No database directory provided. Exiting.")
+
+    good = []
+    bad = []
+
+    jgi2taxid = read_species_map("data/jgi_species_map.csv")
+
+    records = load_sequences(options.input, "fasta")
+    for record in records:
+        record_db = identify_database(record)
+        if record_db == "pfam":
+            pfamseq_acc = record.id
+            domain_number = re.findall(
+                r"domain.*\[*(\d)\]*$", record.description
+            )[0]
+            res = query_database(
+                os.path.join(options.dbdir, PFAM),
+                "pfamseq",
+                ["description", "species", "taxonomy"],
+                {"pfamseq_acc": pfamseq_acc},
+            )
+            if res:
+                header = (
+                    pfamseq_acc
+                    + "|"
+                    + res[0][0]
+                    + "|"
+                    + domain_number
+                    + "|"
+                    + str(taxid)
+                    + "|"
+                    + res[0][1]
+                    + "|"
+                    + res[0][2]
+                )
+                record.id = "pfam|" + header
+                record.description = ""
+                record.name = ""
+            else:
+                sys.exit("Pfam accession %s not found." % str(pfamseq_acc))
+        elif record_db == "ncbi":
+            accession_version = record.id.split("  ")[0]
+            description = "".join(record.description.split("  ")[1:])
+            domain_number = re.findall(
+                r"domain.*\[*(\d)\]*$", record.description
+            )[0]
+            res = query_database(
+                os.path.join(options.dbdir, ACCESSION2TAXID),
+                "nucl_gb",
+                ["taxid"],
+                {"accession.version": accession_version},
+            )
+            if res:
+                taxid = res[0][0]
+            else:
+                sys.exit(
+                    "NCBI accession.version %s not found."
+                    % str(accession_version)
+                )
+            res = query_database(
+                os.path.join(options.dbdir, TAXONOMY),
+                "taxonomy",
+                ["species", "lineage"],
+                {"taxid": taxid},
+            )
+            if res:
+                header = (
+                    accession_version
+                    + "|"
+                    + description
+                    + "|"
+                    + domain_number
+                    + "|"
+                    + str(taxid)
+                    + "|"
+                    + res[0][0]
+                    + "|"
+                    + res[0][1]
+                )
+                record.id = "ncbi|" + header
+                record.description = ""
+                record.name = ""
+            else:
+                print(record)
+                sys.exit("NCBI taxid %s not found." % str(taxid))
+        elif record_db == "jgi":
+            fields = record.description.split("|")
+            species_id = fields[1]
+            if species_id == "Neute_mat_a1":  # hard code this for now...
+                continue
+            transcript_id = fields[2]
+            taxid = jgi2taxid[species_id]
+            protein_id = fields[3]
+            if len(fields) == 4:
+                domain_number = re.findall(r"domain.*\[*(\d)\]*$", protein_id)[
+                    0
+                ]
+            elif len(fields) == 5:
+                domain_number = re.findall(r"domain.*\[*(\d)\]*$", fields[4])[
+                    0
+                ]
+            species_db = os.path.join(
+                options.dbdir, "jgi/" + species_id + "_kog.db"
+            )
+
+            res = query_database(
+                species_db,
+                species_id,
+                ["kogdefline"],
+                {"transcriptID": transcript_id},
+            )
+            if res:
+                protein_description = res[0][0] + "|" + domain_number
+            else:
+                protein_description = (
+                    re.split(r"_*domain", protein_id)[0] + "|" + domain_number
+                )
+                bad.append(record)
+
+            res = query_database(
+                os.path.join(options.dbdir, TAXONOMY),
+                "taxonomy",
+                ["species", "lineage"],
+                {"taxid": taxid},
+            )
+            if res:
+                header = (
+                    "jgi|"
+                    + transcript_id
+                    + "|"
+                    + protein_description
+                    + "|"
+                    + str(taxid)
+                    + "|"
+                    + res[0][0]
+                    + "|"
+                    + res[0][1]
+                )
+                record.id = header
+                record.description = ""
+                record.name = ""
+            else:
+                print(record)
+                sys.exit("NCBI taxid %s not found." % str(taxid))
+        else:
+            sys.exit(
+                "Cannot match %s to an existsing database."
+                % record.description
+            )
+
+    with open(options.input + ".an", "w") as handle:
+        SeqIO.write(records, handle, "fasta-2line")
+
+    with open("failure.fasta", "w") as handle:
+        SeqIO.write(bad, handle, "fasta-2line")
+
+
+if __name__ == "__main__":
+    main()
